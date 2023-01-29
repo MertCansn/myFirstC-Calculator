794ad16da353dead25fd2a46c27221b6840eeba7
cansin <sennmertcansin@gmail.com>
2023-01-29 20:42:54 +0300

    Calculator

---
+++ calculator/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("İlk Sayıyı Seçiniz: ");
+int sayi = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("İkinci Sayıyı Seçiniz: ");
+int sayi2 = Convert.ToInt32(Console.ReadLine());
+double sonuc = 0;
+Console.WriteLine("Yapılacak İşlemi Seçiniz: ( * )Çarpma, ( / ) bölme, ( + )Toplama, ( - )Çıkartma");
+string islem = Console.ReadLine();
+if (islem == "+")
+{
+    sonuc = sayi + sayi2;
+    Console.WriteLine("Sonuç: " + sonuc);
+}
+else if (islem == "-")
+{
+    sonuc = sayi - sayi2;
+    Console.WriteLine("Sonuç: " + sonuc);
+}
+else if (islem == "*")
+{
+    sonuc = sayi * sayi2;
+    Console.WriteLine("Sonuç: " + sonuc);
+}
+else if (islem == "/")
+{
+    sonuc = sayi / sayi2;
+    Console.WriteLine("Sonuç: " + sonuc);
+}
